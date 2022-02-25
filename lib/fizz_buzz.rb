0599fb68a95c743cfd0600d0fbc04465efed5472
Sreang Rathanak <sreangrathanak@yahoo.com>
2022-02-25 19:31:53 +0700

    Add num divided by 3 and 5

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,8 +1,10 @@
 class FizzBuzz
   def fizz_buzz(num)
+    if num % 3 == 0 && num % 5 == 0
+      return "FizzBuzz"
+    end
     # return "Fizz"
     # return "Buzz"
-    # return "FizzBuzz"
     ""
   end
 end
